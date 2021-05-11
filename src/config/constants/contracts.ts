41ad492e8b73333151242512f76ac8dee4bc8fb7
stanfinance <stan.finance20@gmail.com>
2021-05-11 15:46:57 +0530

    upload

---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,50 @@
+export default {
+  cake: {
+    56: '0xA6227a81CC18b35fEdd709F7FC9d1f3e0acDAdf8',
+    97: '',
+  },
+  masterChef: {
+    56: '0x96F5fe35Ec3F19360608b4f7f0607f72fc70411A',
+    97: '',
+  },
+  cake3: {
+    56: '0xcBA1813Ede683333020cedea7C3b63FbaC28e78e',
+    97: '',
+  },
+  masterChef3: {
+    56: '0x48F4eC8AD1cb16fB45f75733BFF41b4BE7f49B00',
+    97: '',
+  },
+  wbnb: {
+    56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    97: '',
+  },
+  lottery: {
+    56: '',
+    97: '',
+  },
+  lotteryNFT: {
+    56: '',
+    97: '',
+  },
+  mulltiCall: {
+    56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',
+    97: '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412',
+  },
+  busd: {
+    56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+    97: '',
+  },
+  usdt: {
+    56: '0x55d398326f99059ff775485246999027b3197955',
+    97: '',
+  },
+  cake2: {
+    56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
+    97: '',
+  },
+  senzu: {
+    56: '0xcba1813ede683333020cedea7c3b63fbac28e78e',
+    97: '',
+  },
+}
