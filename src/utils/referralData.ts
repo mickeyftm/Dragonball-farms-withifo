49fe05dabee11eec6af970c02929aedebaf96e96
Tanu Goyal <tanu.b.goyal@accenture.com>
2021-05-15 23:29:19 +0530

    referal page

---
+++ src/utils/referralData.ts
@@ -0,0 +1,26 @@
+import ReferralsAbi from '../config/abi/referral.json'
+import { getContract } from './web3'
+
+// contract addresses
+const RefferalContractAddress="0x8afdd969617Dcf55312afeA7f66064E1a4c9599c";
+// constract
+const RefferalContract = getContract(ReferralsAbi,RefferalContractAddress);
+
+export const getReferralCount =async (address: string)=>{
+    const response=  await RefferalContract.methods.referralsCount(address).call();
+    return response;
+}
+export const gettotalReferralCommissions = async (address:string)=>{
+    const response =  await RefferalContract.methods.totalReferralCommissions(address).call();
+    return response;
+}
+export const getReferralData=async(account:string)=>{
+    if(account){
+    if(window.location.href.includes("?ref")){
+      const refAddress = window.location.href.split("=")[1];
+      const response=  await RefferalContract.methods.recordReferral(account,refAddress);
+      console.log('referalupdatedata',response)
+    }
+    }
+
+  }
